31f06bdcafdc396e817f3b46dd60ccc6b69aec4c
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-05 12:15:30 +0700

    u text color

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
     return (
         <nav>
             <div className='flex justify-between item-center bg-black text-white p-4'>
-                <div>Graphio Clone</div>
+                <div>Graphio<span className='text-xs text-blue-400'>Clone</span></div>
                 <ul className='flex gap-4'>
                     <li>Home</li>
                     <li>About</li>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <section>
       <div className='p-4 flex flex-col justify-center items-center px-10'>
         <div>
-          <h1 className='text-3xl font-semibold text-center'>Graphio</h1>
+          <h1 className='text-3xl font-semibold text-center'>Graphio<span className='text-lg text-blue-400'>Clone</span></h1>
           <h1 className='text-xl font-semibold text-center mb-4 text-white/50'>Crafting Digital Experiences</h1>
           <p>From concept to code, we transform your vision into engaging, responsive, and powerful web solutions.</p>
         </div>
