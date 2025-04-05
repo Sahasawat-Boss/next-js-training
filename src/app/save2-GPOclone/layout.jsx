ff4373eb7c335fea984fd369f6e18960bc4f4a76
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-05 11:35:28 +0700

    save: save-GPO Clone

--- src/app/save2-GPOclone/layout.jsx
+++ src/app/save2-GPOclone/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./CSS/globals.css";
-
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+        <Nav/>
         {children}
-
+        <Footer/>
       </body>
     </html>
   );
