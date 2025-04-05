b606a2bcc1e322aa1d2d98ec7983fe9240cc142f
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-06 06:59:52 +0700

    a md and u nav

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import Link from "next/link"
 
 const Nav = () => {
     return (
         <nav>
             <div className='flex justify-between item-center bg-black text-white p-4'>
                 <div>Graphio<span className='text-xs text-blue-400'>Clone</span></div>
-                <ul className='flex gap-4'>
-                    <li>Home</li>
-                    <li>About</li>
-                </ul>
+                <div className='flex gap-4'>
+                    <Link href={'/'}>Home</Link>
+                    <Link href={'/pages/about'}>About</Link>
+                    <Link href={'/pages/blog'}>Blog</Link>
+                </div>
             </div>
         </nav>
     )
